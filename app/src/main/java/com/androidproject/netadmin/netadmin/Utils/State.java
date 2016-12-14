2a585477fdcaec13acf7b4c063b87d7c16e11c5c
rvem <rvembox@gmail.com>
2016-12-14 18:41:24 +0300

    init

---
+++ app/src/main/java/com/androidproject/netadmin/netadmin/Utils/State.java
@@ -0,0 +1,12 @@
+package com.androidproject.netadmin.netadmin.Utils;
+
+/**
+ * Created by roman on 30.11.16.
+ */
+
+public enum State {
+    //Computer is online
+    ONLINE,
+    //Computer is offline
+    OFFLINE
+}
---
+++ app/src/main/java/com/androidproject/netadmin/netadmin/model/Computer.java
@@ -0,0 +1,26 @@
+package com.androidproject.netadmin.netadmin.model;
+
+import com.androidproject.netadmin.netadmin.Utils.State;
+
+import java.net.InetAddress;
+
+/**
+ * Created by roman on 30.11.16.
+ */
+
+public class Computer {
+    private int id;
+    private String ip;
+    private State state;
+    public Computer(int id, String ip) {
+        this.id = id;
+        this.ip = ip;
+        this.state = State.OFFLINE;
+    }
+    public String getIP() {
+        return ip;
+    }
+    public void setState(State state) {
+        this.state = state;
+    }
+}
